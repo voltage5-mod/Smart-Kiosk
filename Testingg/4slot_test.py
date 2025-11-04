edae476bbe263bcc2dcc542ecde641eea29b17b4
voltage5-mod <voltage5.mod@gmail.com>
2025-11-04 20:39:54 +0800

    timer 4 slot test

--- Testingg/4slot_test.py
+++ Testingg/4slot_test.py
@@ -1,19 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-# TM1637 CLOCK shared pin
+# Shared CLK pin
 CLK = 5
 
-# TM1637 DIO pins per slot (based on your pinmap)
+# Individual DIO pins
 DIO_PINS = [16, 21, 20, 4]
 
-# Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(CLK, GPIO.OUT)
+
 for dio in DIO_PINS:
     GPIO.setup(dio, GPIO.OUT)
 
-# TM1637 digit segments
 SEG = [0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f]
 
 def start(clk, dio):
@@ -27,44 +26,71 @@ def stop(clk, dio):
 def write_byte(clk, dio, data):
     for i in range(8):
         GPIO.output(clk,0)
-        GPIO.output(dio,(data>>i)&1)
+        GPIO.output(dio, (data >> i) & 1)
+        time.sleep(0.00002)
         GPIO.output(clk,1)
+        time.sleep(0.00002)
+    
+    # ACK bit
     GPIO.output(clk,0)
     GPIO.setup(dio, GPIO.IN)
-    time.sleep(0.00005)
+    time.sleep(0.00002)
+    GPIO.output(clk,1)
+    time.sleep(0.00002)
     GPIO.setup(dio, GPIO.OUT)
 
-def display_number(dio, num, brightness=1):
-    start(CLK, dio)
-    write_byte(CLK, dio, 0x40)  # auto-address
-    stop(CLK, dio)
-    start(CLK, dio)
-    write_byte(CLK, dio, 0xc0)
-
+def show_number(dio, value, brightness=1):
     digits = [
-        SEG[(num//1000)%10],
-        SEG[(num//100)%10],
-        SEG[(num//10)%10],
-        SEG[num%10]
+        SEG[(value//1000)%10],
+        SEG[(value//100)%10],
+        SEG[(value//10)%10],
+        SEG[value%10]
     ]
+
+    # Command: auto-increment address
+    start(CLK, dio)
+    write_byte(CLK, dio, 0x40)
+    stop(CLK, dio)
+
+    # Set starting address 0xC0
+    start(CLK, dio)
+    write_byte(CLK, dio, 0xC0)
     for d in digits:
         write_byte(CLK, dio, d)
     stop(CLK, dio)
 
-    # Set brightness (0-7) low to protect your eyes 😎
+    # Set brightness
     start(CLK, dio)
     write_byte(CLK, dio, 0x88 + brightness)
     stop(CLK, dio)
 
+def clear_display(dio):
+    start(CLK, dio)
+    write_byte(CLK, dio, 0x40)
+    stop(CLK, dio)
+
+    start(CLK, dio)
+    write_byte(CLK, dio, 0xC0)
+    for _ in range(4):
+        write_byte(CLK, dio, 0x00)
+    stop(CLK, dio)
+
+    start(CLK, dio)
+    write_byte(CLK, dio, 0x88)
+    stop(CLK, dio)
+
 try:
-    # Start values for each slot
     timers = [1000, 2000, 3000, 4000]
 
     while True:
         for i, dio in enumerate(DIO_PINS):
-            display_number(dio, timers[i], brightness=1)
+            show_number(dio, timers[i], brightness=1)
             timers[i] = max(0, timers[i] - 1)
+
         time.sleep(1)
 
 except KeyboardInterrupt:
+    for dio in DIO_PINS:
+        clear_display(dio)
     GPIO.cleanup()
+    print("\nDisplays cleared & GPIO cleaned up.")
